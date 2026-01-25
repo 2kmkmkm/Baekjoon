a3dd4c263a42b55ad1e93f12d624451959525827
2kmkmkm <160567391+2kmkmkm@users.noreply.github.com>
2026-01-25 16:15:37 +0900

    [Bronze V] Title: 고양이, Time: 88 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+console.log("\\    /\\");
+console.log(" )  ( \')");
+console.log("(  /  )");
+console.log(" \\(__)|");
